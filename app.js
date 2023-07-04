b4616fe7be6207c7012d8c665d1f4741aeeed574
BoxunW <tony1572002@gmail.com>
2023-07-05 00:30:45 +0800

    feat: add static files & link font awesome

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
+
 // routes setting
 app.get('/', (req, res) => {
   res.render('index')
